2e2b274bc1b9964b7d18998e4c43d0f71c6acc9b
Tbauer426 <bauertyler11@gmail.com>
2021-12-06 23:16:11 -0800

    Went over class wrapping

---
+++ src/Components/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={props.expense[0].title}
+        amount={props.expense[0].amount}
+        date={props.expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[1].title}
+        amount={props.expense[1].amount}
+        date={props.expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[2].title}
+        amount={props.expense[2].amount}
+        date={props.expense[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[3].title}
+        amount={props.expense[3].amount}
+        date={props.expense[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
